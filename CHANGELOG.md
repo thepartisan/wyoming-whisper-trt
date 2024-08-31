4a103aaed3e9524023e5e44a8d80089a06c73328
JonahMMay <jonah@mayfamily.me>
2024-08-31 08:57:15 -0500

    Begin merge of wyoming and whisper_trt

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+## 2.2.0
+
+- Bump faster-whisper package to 1.3.0
+
+## 2.1.0
+
+- Added `--initial-prompt` (see https://github.com/openai/whisper/discussions/963)
+
+## 2.0.0
+
+- Use faster-whisper PyPI package
+- `--model` can now be a HuggingFace model like `Systran/faster-distil-whisper-small.en`
+
+## 1.1.0
+
+- Fix enum use for Python 3.11+
+- Add tests and Github actions
+- Bump tokenizers to 0.15
+- Bump wyoming to 1.5.2
+
+## 1.0.0
+
+- Initial release
+
--- setup.py
+++ setup.py
@@ -1,31 +1,51 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: MIT
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
+#!/usr/bin/env python3
+from pathlib import Path
 
+import setuptools
+from setuptools import setup
 
-from setuptools import setup, find_packages
-from whisper_trt.__version__ import __version__
+this_dir = Path(__file__).parent
 
+requirements = []
+requirements_path = this_dir / "requirements.txt"
+if requirements_path.is_file():
+    with open(requirements_path, "r", encoding="utf-8") as requirements_file:
+        requirements = requirements_file.read().splitlines()
+
+module_name = "wyoming_whisper_trt"
+module_dir = this_dir / module_name
+data_files = []
+
+version_path = module_dir / "VERSION"
+data_files.append(version_path)
+version = version_path.read_text(encoding="utf-8").strip()
+
+# -----------------------------------------------------------------------------
 
 setup(
-    name="whisper_trt",
-    version=__version__,
-    packages=find_packages()
+    name=module_name,
+    version=version,
+    description="Wyoming Server for Whisper powered by TensorRT",
+    url="https://github.com/JonahMMay/wyoming-whisper-trt",
+    author="Jonah May",
+    author_email="mike@rhasspy.org",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    package_data={module_name: [str(p.relative_to(module_dir)) for p in data_files]},
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="rhasspy wyoming whisper stt",
+    entry_points={
+        "console_scripts": ["wyoming-whisper-trt = wyoming_whisper_trt.__main__:run"]
+    },
 )
