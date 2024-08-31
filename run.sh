0cae47eee9b239a2e0980a425ce5455ac5a9dd86
JonahMMay <jonah@mayfamily.me>
2024-08-31 17:34:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /usr/src/ && 
-python3 wyoming-whisper-trt/script/run --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data
+python3 ./wyoming-whisper-trt/script/run --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data
