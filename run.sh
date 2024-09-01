f6568fb2915abd3f03d701fcb3bb4847e7aa2e38
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2024-09-01 16:12:48 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Navigate to the application directory
-cd /usr/src/wyoming-whisper-trt/wyoming-whisper-trt && python3 __main__.py --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug
+cd /usr/src/wyoming-whisper-trt/wyoming_whisper_trt && python3 __main__.py --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug
