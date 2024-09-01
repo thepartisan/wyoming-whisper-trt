7bc653af48b42a6fd55ed8b8d6de159118012ecf
JonahMMay <jonah@mayfamily.me>
2024-08-31 21:12:46 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+# Activate the virtual environment
+source /usr/src/wyoming-whisper-trt/.venv/bin/activate
+
+# Run the script with the appropriate arguments
 /usr/src/wyoming-whisper-trt/script/run --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug
