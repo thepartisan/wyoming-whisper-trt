15dd265d66ab43040f6bd10a9f22086af432b1ec
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2024-09-01 14:03:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-# Ensure the correct working directory is set
-WORKDIR /usr/src/wyoming-whisper-trt
+#!/bin/bash
 
-# Run the application using the module option
-CMD ["python3", "-m", "wyoming_whisper_trt", "--model", "tiny.en", "--language", "en", "--uri", "tcp://0.0.0.0:10300", "--data-dir", "/data", "--download-dir", "/data", "--debug"]
+# Navigate to the application directory
+cd /usr/src/wyoming-whisper-trt
+
+# Run the application
+python3 -m wyoming_whisper_trt --model tiny.en --language en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug
