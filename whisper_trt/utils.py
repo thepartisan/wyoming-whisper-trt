cc81a05ab07251fccae80119925d0b2b0ffe2abd
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2024-12-27 21:37:39 -0500

    Update utils.py

--- whisper_trt/utils.py
+++ whisper_trt/utils.py
@@ -1,15 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
-# SPDX-License-Identifier: MIT
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# [License text continues...]
-
 import hashlib
 from pathlib import Path
 from typing import Optional
